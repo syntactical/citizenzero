bd71676e9f6da1b642e15d478118c910a3a849f2
Greg Dutcher <gdutcher@thoughtworks.com>
2018-02-28 14:59:22 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <a href="eligibility_quiz.html">Click here to start your application.</a>
+      </div>
+    );
+  }
+}
+
+export default App;
